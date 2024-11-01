5c2de08c2d98d1c39a715492ebbaf385ec6ac328
omnidox <hidalgor@montclair.edu>
2024-11-01 13:54:15 -0400

    update2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math_tools
 
 if __name__ == "__main__":
+    print("This is printing from main")
     print(math_tools.square(5))  # Output: 25
     print(math_tools.cube(3))    # Output: 27
 
--- math_tools.py
+++ math_tools.py
@@ -3,3 +3,8 @@ def square(num):
 
 def cube(num):
     return num * num * num
+
+if __name__ == "__main__":
+    print("This is printing from module")
+    print(square(5))  # Output: 25
+    print(cube(3))    # Output: 27
