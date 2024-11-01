6a10fe8f3faaf5c4eb5e95c55b9af3d8356d8501
omnidox <hidalgor@montclair.edu>
2024-11-01 13:44:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math_tools
-print(math_tools.square(5))  # Output: 25
-print(math_tools.cube(3))    # Output: 27
+
+if __name__ == "__main__":
+    print(math_tools.square(5))  # Output: 25
+    print(math_tools.cube(3))    # Output: 27
 
 
